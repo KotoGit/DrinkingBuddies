839ce5d325222d360e5bf6d9578df09d72c5877f
ake1998 <51679263+ake1998@users.noreply.github.com>
2019-07-26 10:29:35 -0400

    Update FilterFragment.java

--- app/src/main/java/com/example/drinkingbuddies/FilterFragment.java
+++ app/src/main/java/com/example/drinkingbuddies/FilterFragment.java
@@ -1,11 +1,16 @@
 package com.example.drinkingbuddies;
+package edu.fsu.cs.drinkingbuddy;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.util.Log;
+import java.util.Timer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,8 +19,9 @@ import android.widget.Toast;
 public class FilterFragment extends Fragment{
     private Button queryButton;
     private EditText dollarAmount;
+    private TextView loadingBars;
     private String dollar;
-
+    
     public FilterFragment() {
         // Required empty public constructor
     }
@@ -32,6 +38,7 @@ public class FilterFragment extends Fragment{
 
         queryButton = (Button) rootView.findViewById(R.id.query_button);
         dollarAmount = (EditText) rootView.findViewById(R.id.query_amount);
+        loadingBars = (TextView) rootView.findViewById(R.id.loading_text);
 
        // Log.d("onCreateView","Here");
 
@@ -39,12 +46,25 @@ public class FilterFragment extends Fragment{
             @Override
             public void onClick(View view) {
                // Toast.makeText( getActivity(), "Hello", Toast.LENGTH_LONG ).show();
+
+                // Animation after being clicked
+                RotateAnimation rotateAnimation = new RotateAnimation(0, 360);
+                rotateAnimation.setDuration(1000);
+                rotateAnimation.setInterpolator(new DecelerateInterpolator());
+                queryButton.startAnimation(rotateAnimation);
+
                 if ( !dollarAmount.getText().toString().equals("") )
                 {
+
+                    loadingBars.setVisibility(View.VISIBLE);
+                    loadingBars.setText("Loading bars. . .");
+
                     dollar = dollarAmount.getText().toString();
                     int dollarInt = Integer.parseInt(dollar);
 
                     Toast.makeText(getActivity(), "Amount is $" + dollarInt, Toast.LENGTH_SHORT).show();
+
+                    loadingBars.setVisibility(View.INVISIBLE);
                 }
 
                 else
