1ce594c9e91813084ed6753f4723a31c2ed093fa
KotoGit <gsreceiver@gmail.com>
2019-07-26 18:26:27 -0400

    fix SQL logic and add empty UserInterface Activity

--- app/src/main/java/com/example/drinkingbuddies/BarProvider.java
+++ app/src/main/java/com/example/drinkingbuddies/BarProvider.java
@@ -3,6 +3,7 @@ package com.example.drinkingbuddies;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -11,7 +12,6 @@ import android.net.Uri;
 public class BarProvider extends ContentProvider {
     public static final int DBVERSION = 1;
     public static final String DBNAME = "BARDB";
-    public static final Uri CONTENT_URI = Uri.parse("content://com.example.drinkingbuddies.provider");
     DataHelper mDataHelper;
 
     //Login table constants
@@ -36,6 +36,9 @@ public class BarProvider extends ContentProvider {
             COLUMN_NAME + " TEXT," + COLUMN_COORDINATES + " TEXT," +
             COLUMN_HOUR + " TEXT," + COLUMN_PRICE + " TEXT)";
 
+    public static final String PROVIDER = "com.example.drinkingbuddies.provider";
+    public static final Uri CONTENT_URI_LOC = Uri.parse("content://com.example.drinkingbuddies.provider" + TABLE_LOCATIONTABLE);
+    public static final Uri CONTENT_URI_LOG = Uri.parse("content://com.example.drinkingbuddies.provider" + TABLE_LOGINTABLE);
     protected static final class DataHelper extends SQLiteOpenHelper{
         DataHelper(Context context){
             super(context, DBNAME, null, DBVERSION);
@@ -50,21 +53,29 @@ public class BarProvider extends ContentProvider {
 
         }
     }
+    private static final int LOGIN = 1;
+    private static final int LOGIN_ID = 2;
+    private static final int LOCATION = 3;
+    private static final int LOCATION_ID = 4;
+    private static final UriMatcher uriMatcher;
+    static{
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(PROVIDER, TABLE_LOGINTABLE, LOGIN);
+        uriMatcher.addURI(PROVIDER, TABLE_LOGINTABLE + "/#", LOGIN_ID);
+        uriMatcher.addURI(PROVIDER, TABLE_LOCATIONTABLE, LOCATION);
+        uriMatcher.addURI(PROVIDER, TABLE_LOCATIONTABLE + "/#", LOCATION_ID);
+    }
 
-    //use the Uri to get the table names
     /****** DELETE METHODS ******/
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        //program uses different delete methods
-        return 0;
-    }
-
-    public int delete_login(Uri uri, String selection, String[] selectionArgs){
-        return mDataHelper.getWritableDatabase().delete(TABLE_LOGINTABLE, selection, selectionArgs);
-    }
-
-    public int delete_location(Uri uri, String selection, String[] selectionArgs){
-        return mDataHelper.getWritableDatabase().delete(TABLE_LOCATIONTABLE, selection, selectionArgs);
+        switch(uriMatcher.match(uri)){
+            case LOGIN:
+                return mDataHelper.getWritableDatabase().delete(TABLE_LOGINTABLE, selection, selectionArgs);
+            case LOCATION:
+                return mDataHelper.getWritableDatabase().delete(TABLE_LOCATIONTABLE, selection, selectionArgs);
+            default:return 0;
+        }
     }
 
     @Override
@@ -75,36 +86,33 @@ public class BarProvider extends ContentProvider {
     /****** INSERT METHODS ******/
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        //program uses different insert methods
-        return uri;
-    }
+        switch(uriMatcher.match(uri)){
+            case LOGIN:
+                String username = values.getAsString(COLUMN_USERNAME);
+                String passwd = values.getAsString(COLUMN_PASSWD);
+                if(username == null || passwd == null){
+                    return null;
+                }
+                else if(username.equals("") || passwd.equals("")){
+                    return null;
+                }
 
-    public Uri insert_login(Uri uri, ContentValues values){
-        String username = values.getAsString(COLUMN_USERNAME);
-        String passwd = values.getAsString(COLUMN_PASSWD);
-        if(username == null || passwd == null){
-            return null;
-        }
-        else if(username.equals("") || passwd.equals("")){
-            return null;
-        }
+                long id1 = mDataHelper.getWritableDatabase().insert(TABLE_LOGINTABLE, null, values);
+                return Uri.withAppendedPath(CONTENT_URI_LOG, "" + id1);
+            case LOCATION:
+                String name = values.getAsString(COLUMN_NAME);
+                String coord = values.getAsString(COLUMN_COORDINATES);
+                if(name == null || coord == null){
+                    return null;
+                }
+                else if(name.equals("") || coord.equals("")){
+                    return null;
+                }
 
-        long id = mDataHelper.getWritableDatabase().insert(TABLE_LOGINTABLE, null, values);
-        return Uri.withAppendedPath(CONTENT_URI, "" + id);
-    }
-
-    public Uri insert_location(Uri uri, ContentValues values){
-        String name = values.getAsString(COLUMN_NAME);
-        String coord = values.getAsString(COLUMN_COORDINATES);
-        if(name == null || coord == null){
-            return null;
+                long id2 = mDataHelper.getWritableDatabase().insert(TABLE_LOCATIONTABLE, null, values);
+                return Uri.withAppendedPath(CONTENT_URI_LOC, "" + id2);
+            default: return null;
         }
-        else if(name.equals("") || coord.equals("")){
-            return null;
-        }
-
-        long id = mDataHelper.getWritableDatabase().insert(TABLE_LOCATIONTABLE, null, values);
-        return Uri.withAppendedPath(CONTENT_URI, "" + id);
     }
 
     @Override
@@ -117,51 +125,43 @@ public class BarProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
-        //program uses different query methods
-        return null;
-    }
-
-    public Cursor query_login(Uri uri, String[] projection, String selection,
-                              String[] selectionArgs, String sortOrder){
-        return mDataHelper.getReadableDatabase().query(TABLE_LOGINTABLE, projection, selection, selectionArgs, null, null, sortOrder);
-    }
-
-    public Cursor query_location(Uri uri, String[] projection, String selection,
-                              String[] selectionArgs, String sortOrder){
-        return mDataHelper.getReadableDatabase().query(TABLE_LOCATIONTABLE, projection, selection, selectionArgs, null, null, sortOrder);
+        switch(uriMatcher.match(uri)) {
+            case LOGIN:
+                return mDataHelper.getReadableDatabase().query(TABLE_LOGINTABLE, projection, selection, selectionArgs, null, null, sortOrder);
+            case LOCATION:
+                return mDataHelper.getReadableDatabase().query(TABLE_LOCATIONTABLE, projection, selection, selectionArgs, null, null, sortOrder);
+            default:
+                return null;
+        }
     }
 
     /****** UPDATE METHODS ******/
     @Override
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
-        //program uses different update methods
-        return 0;
-    }
-
-    public int update_login(Uri uri, ContentValues values, String selection,
-                            String[] selectionArgs){
-        String username = values.getAsString(COLUMN_USERNAME);
-        String passwd = values.getAsString(COLUMN_PASSWD);
-        if(username == null || passwd == null){
-            return -1;
+        switch(uriMatcher.match(uri)){
+            case LOGIN:
+                String username = values.getAsString(COLUMN_USERNAME);
+                String passwd = values.getAsString(COLUMN_PASSWD);
+                if(username == null || passwd == null){
+                    return -1;
+                }
+                else if(username.equals("") || passwd.equals("")){
+                    return -1;
+                }
+                return mDataHelper.getWritableDatabase().update(TABLE_LOGINTABLE, values, selection, selectionArgs);
+            case LOCATION:
+                String name = values.getAsString(COLUMN_NAME);
+                String coord = values.getAsString(COLUMN_COORDINATES);
+                if(name == null || coord == null){
+                    return -1;
+                }
+                else if(name.equals("") || coord.equals("")){
+                    return -1;
+                }
+                return mDataHelper.getWritableDatabase().update(TABLE_LOCATIONTABLE, values, selection, selectionArgs);
+            default:
+                return 0;
         }
-        else if(username.equals("") || passwd.equals("")){
-            return -1;
-        }
-        return mDataHelper.getWritableDatabase().update(TABLE_LOGINTABLE, values, selection, selectionArgs);
-    }
-
-    public int update_location(Uri uri, ContentValues values, String selection,
-                            String[] selectionArgs){
-        String name = values.getAsString(COLUMN_NAME);
-        String coord = values.getAsString(COLUMN_COORDINATES);
-        if(name == null || coord == null){
-            return -1;
-        }
-        else if(name.equals("") || coord.equals("")){
-            return -1;
-        }
-        return mDataHelper.getWritableDatabase().update(TABLE_LOCATIONTABLE, values, selection, selectionArgs);
     }
 }
---
+++ app/src/main/java/com/example/drinkingbuddies/UserInterface.java
@@ -0,0 +1,16 @@
+package com.example.drinkingbuddies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_interface);
+    }
+
+
+}
