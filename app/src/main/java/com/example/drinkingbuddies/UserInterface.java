caa6312224ecf74b5110db2e69a25e1825eb1fa2
KotoGit <gsreceiver@gmail.com>
2019-07-29 17:58:11 -0400

    complete login and register logic

--- app/src/main/java/com/example/drinkingbuddies/UserInterface.java
+++ app/src/main/java/com/example/drinkingbuddies/UserInterface.java
@@ -3,13 +3,23 @@ package com.example.drinkingbuddies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class UserInterface extends AppCompatActivity {
+    TextView userTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_interface);
+        Bundle extras = getIntent().getExtras();
+        String username = "";
+        if(extras != null) {
+            username = extras.getString("user");
+        }
+        userTitle = (TextView)findViewById(R.id.user_text);
+        String welcomeText = "Welcome " + username + "!";
+        userTitle.setText(welcomeText);
     }
 
 
