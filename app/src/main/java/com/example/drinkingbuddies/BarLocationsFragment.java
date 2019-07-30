b62906a39b7202b5bcf9cb5c00f26ebf5f65ef9d
Cody Ickes <cody.ickes@hotmail.com>
2019-07-30 16:50:21 -0400

    API fully functional. Fragment obtains list of bars

--- app/src/main/java/com/example/drinkingbuddies/BarLocationsFragment.java
+++ app/src/main/java/com/example/drinkingbuddies/BarLocationsFragment.java
@@ -34,80 +34,77 @@ public class BarLocationsFragment extends Fragment {
 
     private final String URL = "https://developers.zomato.com/api/v2.1/";
     private final String API_KEY = "dd755fc4ccc5e5e90d898f165326aa4a";
+
+    // Set to desired location to get bars in area
+    private final String LOCATION_QUERY = "";
+
     public JSONObject jsonObject;
     public JSONArray jsonArray;
-    private String[] keys, values;
     RequestQueue queue;
-    JsonArrayRequest jsonArrayRequest;
     JsonObjectRequest jsonObjectRequest;
+    List<String> barList = new ArrayList<>();
+    String cityID;
+    String establishmentID = new String();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         queue = Volley.newRequestQueue(getContext());
 
-        List<String> bars = findBars("Jacksonville");
-        for(int i = 0; i < bars.size(); i++) {
-            Log.v("City", bars.get(i));
-        }
+        // Populate barList with all bars found within LOCATION_QUERY
+        apiObjectQuery("cities", new String[]{"q"}, new String[]{LOCATION_QUERY});
 
         return inflater.inflate(R.layout.fragment_bar_locations, container, false);
     }
 
     // Saves JSON result to json variable in Fragment
-    public void apiArrayQuery(String resource, String[] k, String[] v) {
+    public void apiObjectQuery(final String resource, String[] k, String[] v) {
 
-        keys = k;
-        values = v;
-
-        jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, URL + resource, null, new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
-                jsonArray = response;
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                // TODO: Handle error
-                Log.v("ERROR", error.toString());
-            }
-        }) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> headers = new HashMap<>();
-
-                headers.put("Accept", "application/json");
-                headers.put("user-key", API_KEY);
-
-                for(int i = 0; i < keys.length; i++) {
-                    headers.put(keys[i], values[i]);
-                }
-
-                return headers;
-            }
-        };
-
-        queue.add(jsonArrayRequest);
-    }
-
-    public void apiObjectQuery(String resource, String[] k, String[] v) {
-
-        keys = k;
-        values = v;
-
-        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL + resource, null, new Response.Listener<JSONObject>() {
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL + resource + addQuery(k, v), null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 jsonObject = response;
                 try {
-                    jsonArray = jsonObject.getJSONArray("location_suggestions");
-                    Log.v("ARRAY", jsonArray.toString());
-                    jsonObject = jsonArray.getJSONObject(0);
+                    // Get cityID
+                    if(resource.matches("cities")) {
+                        jsonArray = jsonObject.getJSONArray("location_suggestions");
+
+                        if(!jsonArray.isNull(0))
+                            jsonObject = jsonArray.getJSONObject(0);
+                        else
+                            return;
+
+                        cityID = jsonObject.getString("id");
+                        apiObjectQuery("establishments", new String[]{"city_id"}, new String[]{cityID});
+                    }
+
+                    // Get establishmentID
+                    else if(resource.matches("establishments")) {
+                        jsonArray = jsonObject.getJSONArray("establishments");
+
+                        for(int i = 0; i < jsonArray.length(); i++) {
+                            if(jsonArray.getJSONObject(i).getJSONObject("establishment").getString("name").equalsIgnoreCase("bar")) {
+                                establishmentID = jsonArray.getJSONObject(i).getJSONObject("establishment").getString("id");
+                            }
+                        }
+
+                        apiObjectQuery("search", new String[]{"entity_id", "entity_type", "establishment_type"}, new String[]{cityID, "city", establishmentID});
+                    }
+
+                    // Get barList
+                    else if(resource.matches("search")) {
+                        jsonArray = jsonObject.getJSONArray("restaurants");
+                        for(int i = 0; i < jsonArray.length(); i++) {
+                            barList.add(jsonArray.getJSONObject(i).getJSONObject("restaurant").getString("name"));
+                        }
+
+                        for(int i = 0; i < barList.size(); i++) {
+                            Log.v("BAR", barList.get(i));
+                        }
+                    }
                 }
                 catch(JSONException e) {
-
+                    Log.v("ERROR", e.getMessage());
                 }
-
-                Log.v("CITY", jsonObject.toString());
             }
         }, new Response.ErrorListener() {
             @Override
@@ -122,10 +119,6 @@ public class BarLocationsFragment extends Fragment {
 
                 headers.put("Accept", "application/json");
                 headers.put("user-key", API_KEY);
-
-                for(int i = 0; i < keys.length; i++) {
-                    headers.put(keys[i], values[i]);
-                }
 
                 return headers;
             }
@@ -134,37 +127,14 @@ public class BarLocationsFragment extends Fragment {
         queue.add(jsonObjectRequest);
     }
 
-    public List<String> findBars(String location) {
-        List<String> barList = new ArrayList<>();
-        String cityID;
-        String establishmentID = new String();
+    public String addQuery(String[] k, String[] v) {
+        StringBuilder sb = new StringBuilder();
 
-        apiObjectQuery("cities", new String[]{"q"}, new String[]{location});
+        for(int i = 0; i < k.length; i++) {
+            sb.append("?" + k[i] + "=");
+            sb.append(v[i].replace(" ", "%20"));
+        }
 
-        /*
-        try {
-            jsonArray = jsonObject.getJSONArray("location_suggestions");
-            jsonObject = jsonArray.getJSONObject(0);
-            Log.v("CITY", jsonObject.toString());
-            cityID = jsonObject.getString("id");
-
-            apiArrayQuery("establishments", new String[]{"city_id"}, new String[]{cityID});
-            for(int i = 0; i < jsonArray.length(); i++) {
-                if(jsonArray.getJSONObject(i).getString("establishment_name").equalsIgnoreCase("bar"))
-                    establishmentID = jsonArray.getJSONObject(i).getString("establishment_id");
-            }
-
-            apiArrayQuery("search", new String[]{"entity_id", "entity_type", "establishment_type"}, new String[]{cityID, "city", establishmentID});
-            jsonArray = jsonArray.getJSONObject(0).getJSONArray("restaurants");
-            for(int i = 0; i < jsonArray.length(); i++) {
-                barList.add(jsonArray.getJSONObject(i).getString("name"));
-            }
-
-        } catch(JSONException e) {
-            return null;
-        }*/
-
-
-        return barList;
+        return sb.toString();
     }
 }
